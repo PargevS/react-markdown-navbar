49b114d5d04c6d9444eff120c9a8c380e96fe74d
P.Sargsyan <psargsyan.dev@gmail.com>
2021-12-12 04:25:21 +0300

    initial

---
+++ src/article.js
@@ -0,0 +1,177 @@
+const article = `# Markdown-Navbar Demo
+
+The current page is a demo for markdown-navbar You can click **the button below** to edit or debug it on *codesandbox.io*.
+
+[![Edit markdown-navbar-demo-online](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/markdown-navbar-demo-online-e7e0n?fontsize=14&hidenavigation=1&theme=dark)
+
+## Title 1
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+* Chicken Chicken Chicken Chicken Chicken.
+* Chicken Chicken Chicken Chicken Chicken.
+* Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+### Title 2
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+#### Title 4
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 5
+
+Chicken Chicken Chicken Chicken Chicken.
+
+### Title 6
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+#### Title 7
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 8
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 9
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 10
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 11
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 12
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 13
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 14
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 15
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 16
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+## Title 17
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
+
+![react logo](/logo512.png)
+`;
+
+export default article;
